e2d83a97ef974ce280caa6a098adaeffd10bc90d
Tushar Bhatia <91830386+tushar1003@users.noreply.github.com>
2023-09-13 14:22:52 +0530

    Update EditProfile.js

--- src/pages/profile/EditProfile.js
+++ src/pages/profile/EditProfile.js
@@ -1,14 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import Card from "../../components/card/Card";
-import Loader from "../../components/loader/Loader";
-import { selectUser } from "../../redux/features/auth/authSlice";
-import "./Profile.scss";
-import { toast } from "react-toastify";
-import { updateUser } from "../../services/authService";
-import ChangePassword from "../../components/changePassword/ChangePassword";
+import React, { useEffect, useState, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Card from '../../components/card/Card';
+import Loader from '../../components/loader/Loader';
+import { selectUser } from '../../redux/features/auth/authSlice';
+import './Profile.scss';
+import { toast } from 'react-toastify';
+import { updateUser } from '../../services/authService';
+import ChangePassword from '../../components/changePassword/ChangePassword';
+import Modal from 'react-modal';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    maxWidth: '600px',
+    maxHeight: '90vh',
+    padding: '20px',
+    borderRadius: '8px',
+    boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.2)',
+    overflowY: 'auto',
+  },
+};
 const EditProfile = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
@@ -17,7 +34,7 @@ const EditProfile = () => {
 
   useEffect(() => {
     if (!email) {
-      navigate("/profile");
+      navigate('/profile');
     }
   }, [email, navigate]);
 
@@ -29,7 +46,11 @@ const EditProfile = () => {
     photo: user?.photo,
   };
   const [profile, setProfile] = useState(initialState);
-  const [profileImage, setProfileImage] = useState("");
+  const [profileImage, setProfileImage] = useState('');
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  // Use a ref to access the modal element
+  const modalRef = useRef(null);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -48,22 +69,22 @@ const EditProfile = () => {
       let imageURL;
       if (
         profileImage &&
-        (profileImage.type === "image/jpeg" ||
-          profileImage.type === "image/jpg" ||
-          profileImage.type === "image/png")
+        (profileImage.type === 'image/jpeg' ||
+          profileImage.type === 'image/jpg' ||
+          profileImage.type === 'image/png')
       ) {
         const image = new FormData();
-        image.append("file", profileImage);
-        image.append("cloud_name", "diqbawhl5");
-        image.append("upload_preset", "jt0r8oej");
+        image.append('file', profileImage);
+        image.append('cloud_name', 'diqbawhl5');
+        image.append('upload_preset', 'jt0r8oej');
 
         // First save image to cloudinary
         const response = await fetch(
-          "https://api.cloudinary.com/v1_1/diqbawhl5/image/upload",
-          { method: "post", body: image }
+          'https://api.cloudinary.com/v1_1/diqbawhl5/image/upload',
+          { method: 'post', body: image }
         );
         const imgData = await response.json();
-        console.log(imgData); 
+        console.log(imgData);
         if (imgData.url) {
           imageURL = imgData.url.toString();
         }
@@ -77,8 +98,8 @@ const EditProfile = () => {
 
         const data = await updateUser(formData);
         console.log(data);
-        toast.success("User updated");
-        navigate("/profile");
+        toast.success('User updated');
+        navigate('/profile');
         setIsLoading(false);
       }
     } catch (error) {
@@ -88,36 +109,66 @@ const EditProfile = () => {
     }
   };
 
+  // Function to close the modal
+  // const closeModal = () => {
+  //   setIsFormOpen(false);
+  // };
+
+  // Add a click event listener to the document
+  // useEffect(() => {
+  //   const handleClickOutside = (event) => {
+  //     if (modalRef.current && !modalRef.current.contains(event.target)) {
+  //       closeModal();
+  //     }
+  //   };
+
+  //   // Attach the event listener
+  //   document.addEventListener('click', handleClickOutside);
+
+  //   // Clean up the event listener when the component unmounts
+  //   return () => {
+  //     document.removeEventListener('click', handleClickOutside);
+  //   };
+  // }, []);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  // Close the modal
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
   return (
-    <div className="profile --my2">
+    <div className='profile --my2'>
       {isLoading && <Loader />}
 
-      <Card cardClass={"card --flex-dir-column"}>
-        <span className="profile-photo">
-          <img src={user?.photo} alt="profilepic" />
+      <Card cardClass={'card --flex-dir-column'}>
+        <span className='profile-photo'>
+          <img src={user?.photo} alt='profilepic' />
         </span>
-        <form className="--form-control --m" onSubmit={saveProfile}>
-          <span className="profile-data">
+        <form className='--form-control --m' onSubmit={saveProfile}>
+          <span className='profile-data'>
             <p>
               <label>Name:</label>
               <input
-                type="text"
-                name="name"
+                type='text'
+                name='name'
                 value={profile?.name}
                 onChange={handleInputChange}
               />
             </p>
             <p>
               <label>Email:</label>
-              <input type="text" name="email" value={profile?.email} disabled />
+              <input type='text' name='email' value={profile?.email} disabled />
               <br />
               <code>Email cannot be changed.</code>
             </p>
             <p>
               <label>Phone:</label>
               <input
-                type="text"
-                name="phone"
+                type='text'
+                name='phone'
                 value={profile?.phone}
                 onChange={handleInputChange}
               />
@@ -125,25 +176,53 @@ const EditProfile = () => {
             <p>
               <label>Bio:</label>
               <textarea
-                name="bio"
+                name='bio'
                 value={profile?.bio}
                 onChange={handleInputChange}
-                cols="30"
-                rows="10"
+                cols='30'
+                rows='10'
               ></textarea>
             </p>
             <p>
               <label>Photo:</label>
-              <input type="file" name="image" onChange={handleImageChange} />
+              <input type='file' name='image' onChange={handleImageChange} />
             </p>
             <div>
-              <button className="--btn --btn-primary">Edit Profile</button>
+              <button className='--btn --btn-primary'>Edit Profile</button>
             </div>
           </span>
         </form>
       </Card>
       <br />
-      <ChangePassword />
+      {/* <button
+        onClick={() =>{ setIsFormOpen(true);}}
+        className='add-transaction-button'
+      >
+        Change Password
+      </button> */}
+
+      {/* {isFormOpen && (
+        <div className='modal-overlay'>
+          <div className='modal'>
+            <ChangePassword />
+            <button
+        onClick={() =>{ setIsFormOpen(false);}}
+        className='add-transaction-button'
+      > Cancel change password</button>
+          </div>
+        </div>
+      )} */}
+      <button className='--btn --btn-primary' onClick={openModal}>
+        Change Password
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel='Add Product Modal'
+         style={customStyles}
+      >
+        <ChangePassword />
+      </Modal>
     </div>
   );
 };
